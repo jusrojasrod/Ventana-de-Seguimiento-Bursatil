e7aca5bab7c494d6c9b0b5dc4e2e619e986d1775
jusrojasrod <rebelion.96@gmail.com>
2023-11-16 12:08:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_namespace_packages
 
+
+VERSION = "0.0.1"
+
+
 setup(name='vensebur',
-      packages=find_namespace_packages())
+      packages=find_namespace_packages(),
+      version=VERSION,
+      author="Sebastian Rojas")
