35b1d41022e663bd42cd93f5326db58340fcb465
jusrojasrod <rebelion.96@gmail.com>
2023-11-23 12:39:44 -0500

    changed parameter

--- vensebur/EODHDdata/data.py
+++ vensebur/EODHDdata/data.py
@@ -104,7 +104,7 @@ def downloadAllTickers(tickers, start, end, period='d', _filter=None,
         Group by 'ticker' or 'column' (default)
 
     """
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(partial(handle_data, start=start, end=end,
                              period=period, _filter=_filter),
                      tickers)
